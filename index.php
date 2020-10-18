7437d569affd6f522d97dc3d421d897de98f4709
Tori Andika Bukit <23517015@std.stei.itb.ac.id>
2020-10-18 15:15:47 +0700

    update slide show

--- index.php
+++ index.php
@@ -1,29 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <title>LDR Opswork Web</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #333;}</style>
-        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    </head>
+  <style>
+    body {
+      background-color:black;
+    }
+    h2 {
+      color: white;
+    }
+    p {
+      color: white;
+    }
+  </style>
+</head>
+<body>
 
-    <body>
-        <div class="container">
-            <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations!</h2>
-                <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
-                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
-            </div>
+<div class="container">
+  <h2>Lana del Rey Poster</h2>
+  <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; .</p>
+  <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0"></li>
+      <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item ">
+        <img src="images/ldr1.png" alt="NFR" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>NFR Cover Album Alt</h3>
+          <p>This is art work for NFR album!</p>
         </div>
+      </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-    </body>
+      <div class="item active">
+        <img src="images/ldr2.png" alt="Bogota" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Bogota Music Festival</h3>
+          <p>Have so much fun in Colombia!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="images/ldr3.png" alt="Witch" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Witch</h3>
+          <p>The only witch we all love!</p>
+        </div>
+      </div>
 
+      <div class="item">
+        <img src="images/ldr4.jpg" alt="LDRXTarantino" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>LDR X Tarantino</h3>
+          <p>What if Pulp Fiction starred by Lana?</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+</body>
 </html>
